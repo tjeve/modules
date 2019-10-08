31e594436c34fcd6c0326d1e6733dc9422759be2
Terrence Eve <tijayeve@gmail.com>
2019-10-08 18:38:10 -0500

    Completed parts 1 and 2 of modules exercises

---
+++ index.js
@@ -0,0 +1,14 @@
+/* require_statements_1 */
+// const file1 = require('./require_statements_1/file1.js')
+// const file2 = require('./require_statements_1/file2.js')
+// const file3 = require('./require_statements_1/file3.js')
+
+/* require_statements_2 */
+// const file1 = require('./require_statements_2/file1.js')
+// const file2 = require('./require_statements_2/file2.js')
+// const file3 = require('./require_statements_2/file3.js')
+
+// console.log(file1 + ", " + file2 + " is a number, " + "and a boolean can be " + file3 + " or false")
+
+/* require_statements_3 */
+
--- require_statements_1/index.js
+++
@@ -1,7 +0,0 @@
-const file1 = require('./modules/require_statements_1/file1.js')
-const file2 = require('./require_statements_1/file1.js')
-const file3 = require('./require_statements_1/file1.js')
-
-file1()
-file2()
-file3()
