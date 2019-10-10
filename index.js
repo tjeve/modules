22e3940a155310c5c295cce80c70e19ab8034dd7
Terrence Eve <tijayeve@gmail.com>
2019-10-10 00:12:22 -0500

    Used all modules, but need to fix if/else statement

--- index.js
+++ index.js
@@ -11,4 +11,28 @@
 // console.log(file1 + ", " + file2 + " is a number, " + "and a boolean can be " + file3 + " or false")
 
 /* require_statements_3 */
+// const array = require('./require_statements_3/array.js')
+
+// for (var idx = 0; idx < array.length; idx++) {
+//     console.log(array[idx])
+// }
+
+// /* require_statements_4 */
+// const add = require('./require_statements_4/add.js')
+
+// console.log(add(1, 1))
+// console.log(add(200, 100))
+// console.log(add(123, 321))
+
+/* require_statements_5 */
+
+const dog = require('./require_statements_5/dog.js')
+const cat = require('./require_statements_5/cat.js')
+const randomValue = require('./require_statements_5/randomValue.js')
+
+console.log(randomValue(2, 5))
+if (randomValue < 5) {
+    return console.log(cat)
+} else {return console.log(dog)}
+console.log(randomValue)
 
